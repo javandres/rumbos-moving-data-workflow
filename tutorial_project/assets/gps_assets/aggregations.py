e3f5bb6abd93eea2659ec96489e71f0d686cfe7b
Javier Andrés García <javier.garcia@foodgraph.com>
2024-02-11 10:21:12 -0500

    add all code aggregations assets

--- tutorial_project/assets/gps_assets/aggregations.py
+++ tutorial_project/assets/gps_assets/aggregations.py
@@ -19,6 +19,7 @@ import movingpandas as mpd
 from datetime import datetime, time, timedelta
 
 
+
 url = URL.create(
     "postgresql+psycopg2",
     username=os.getenv("POSTGRES_USER"),
@@ -29,6 +30,58 @@ url = URL.create(
 )
 conn = create_engine(url).connect()
 positionfixes_table = "_positionfixes_with_mode"
+
+@multi_asset(
+    name="all_aggregations",
+    group_name= "aggregations",
+    compute_kind="movingpandas_agg",
+    outs={
+        "agg_flows": AssetOut(
+            key_prefix=["public"],
+            io_manager_key="mobilityDb_manager",
+        ),
+        "agg_clusters": AssetOut(
+            key_prefix=["public"],
+            io_manager_key="mobilityDb_manager",
+        )
+    },
+)
+def asset_template():
+
+    sql = f"""
+        SELECT * 
+        FROM {positionfixes_table} 
+        where type = 'persona'
+        order by tracked_at
+    """
+    gdf = gpd.GeoDataFrame.from_postgis(sql, conn, geom_col="geometry")
+    gdf = gdf.set_index("tracked_at")
+    gdf["time"] = gdf.index
+
+    traj_collection = mpd.TrajectoryCollection(gdf, 'track_id', t="time")
+    aggregator = mpd.TrajectoryCollectionAggregator(traj_collection, max_distance=100, min_distance=10, min_stop_duration=timedelta(minutes=5))
+    clusters = aggregator.get_clusters_gdf()
+    print(clusters.head())
+    flows = aggregator.get_flows_gdf()
+    print(flows.head())    
+    return (
+        Output(
+            value=flows,
+            metadata={
+                "description": "",
+                "rows": 0,
+                "preview": "",
+            },
+        ),
+        Output(
+            value=clusters,
+            metadata={
+                "description": "",
+                "rows": 0,
+                "preview": "",
+            },
+        ),
+    )
 
 
 def make_aggregations_by_code(code):
@@ -106,9 +159,8 @@ aggregations_assets = []
 for track in tracks:
     count += 1
     print("Processing track", track[0], "count", count, "of", size)
-    r = make_aggregations_by_code(track[0])
-    print("============", r)
-    aggregations_assets.append(r)
+    agg_assets = make_aggregations_by_code(track[0])
+    aggregations_assets.append(agg_assets)
 
 
 print("All aggregation assets loaded.")
